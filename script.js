5ef4fff3d6fd0aad12b9bd02726488cc94373d75
bot-dot-anand <64434636+bot-dot-anand@users.noreply.github.com>
2020-05-23 01:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,9 @@ function result(){
 }
 btn.onclick=result;
 btn_hint.onclick=hint;
+input.addEventListener("keyup",function(event){
+	if(event.keyCode===13){
+		event.preventDefault();
+		btn.click();
+	}
+});
